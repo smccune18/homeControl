b0da327af6c42cf78d46088da135cb21952da7cd
Stephen McCune <smccune@gmail.com>
2018-09-09 15:11:08 +0800

    Adding mp3

--- mp3Play.py
+++ mp3Play.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+import random
 
 lovelineMp3s = []
 for dir, subdirs, files in os.walk('/home/pi/Music/LoveLine'):
@@ -8,5 +10,12 @@ for dir, subdirs, files in os.walk('/home/pi/Music/LoveLine'):
 
 for line in lovelineMp3s:
     print line
+
+def play_random(count):
+    counter = 0
+    while counter < count:
+        counter+=1
+        subprocess.Popen(['mpg123', '-q', lovelineMp3s[random.randint(0, len(lovelineMp3s))]]).wait()
+        #play_mp3(random.randint(0, len(lovelineMp3s)))
 
 
